da6d0e6a614e436aac6d3a33e6a8ffe6030800d5
Levi <lv.durana@gmail.com>
2016-05-09 11:09:24 -0300

    Inicialização do projeto, base da janela principal.

---
+++ lab_main.c
@@ -0,0 +1,102 @@
+#include <windows.h>
+#include <tchar.h>
+#include <stdio.h>
+
+#define WINDOW_WIDTH 300
+#define WINDOW_HEIGHT 300
+
+LRESULT CALLBACK WindowProcedure (HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam);
+TCHAR szClassName[] = _T("GMatrizes");
+HWND main_window;
+
+int WINAPI WinMain (HINSTANCE hThisInstance,
+                     HINSTANCE hPrevInstance,
+                     LPSTR lpszArgument,
+                     int nCmdShow)
+{
+    HWND hwnd;
+    MSG messages;
+    WNDCLASSEX wincl;
+
+    wincl.hInstance = hThisInstance;
+    wincl.lpszClassName = szClassName;
+    wincl.lpfnWndProc = WindowProcedure;
+    wincl.style = CS_DBLCLKS;
+    wincl.cbSize = sizeof (WNDCLASSEX);
+    wincl.hIcon = LoadIcon (NULL, IDI_APPLICATION);
+    wincl.hIconSm = NULL;//(HICON)LoadImage(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_MYICON), IMAGE_ICON, 16, 16, 0);
+    wincl.hCursor = LoadCursor (NULL, IDC_ARROW);
+    wincl.lpszMenuName = NULL;//MAKEINTRESOURCE(IDR_MYMENU);
+    wincl.cbClsExtra = 0;
+    wincl.cbWndExtra = 0;
+    wincl.hbrBackground = (HBRUSH) COLOR_BACKGROUND;
+
+    if (!RegisterClassEx (&wincl))
+        return 0;
+
+    hwnd = CreateWindowEx (
+           0,                   /* Extended possibilites for variation */
+           szClassName,         /* Classname */
+           _T("Labirinto do Rato"),       /* Title Text */
+           WS_OVERLAPPEDWINDOW, /* default window */
+           CW_USEDEFAULT,       /* Windows decides the position */
+           CW_USEDEFAULT,       /* where the window ends up on the screen */
+           WINDOW_WIDTH,                 /* The programs width */
+           WINDOW_HEIGHT,                 /* and height in pixels */
+           HWND_DESKTOP,        /* The window is a child-window to desktop */
+           NULL,                /* No menu */
+           hThisInstance,       /* Program Instance handler */
+           NULL                 /* No Window Creation data */
+           );
+
+    ShowWindow (hwnd, nCmdShow);
+    main_window = hwnd;
+
+    while (GetMessage (&messages, NULL, 0, 0))
+    {
+        TranslateMessage(&messages);
+        DispatchMessage(&messages);
+    }
+
+    return messages.wParam;
+
+};
+
+LRESULT CALLBACK WindowProcedure (HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+
+
+
+    switch (message)
+    {
+        case WM_CREATE:
+            {
+                UpdateWindow(hwnd);
+            };
+            break;
+
+        case WM_PAINT:
+        {
+
+        }
+        break;
+        case WM_COMMAND:
+            switch(LOWORD(wParam))
+            {
+
+            }
+            break;
+        case WM_LBUTTONDOWN:
+            {
+
+            }
+        break;
+        case WM_DESTROY:
+            PostQuitMessage (0);
+            break;
+        default:
+            return DefWindowProc (hwnd, message, wParam, lParam);
+    }
+
+    return 0;
+};
